79a5eab5d1d47c6fbc0cf35351827fd557c3247e
NarinaHarutyunyan <nari_har@yahoo.com>
2022-09-11 17:10:48 +0400

    Game 'Memory Card'

---
+++ main.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let counter = 0;
+let firstSelection = "";
+let secondSelection = "";
+
+const cards = document.querySelectorAll(".cards .card");
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    card.classList.add("clicked");
+
+    if (counter === 0) {
+      firstSelection = card.getAttribute("diamond");
+      counter++;
+    } else {
+      secondSelection = card.getAttribute("diamond");
+      counter = 0;
+
+      if (firstSelection === secondSelection) {
+        const correctCards = document.querySelectorAll(
+          ".card[diamond='" + firstSelection + "']"
+        );
+
+        correctCards[0].classList.add("checked");
+        correctCards[0].classList.remove("clicked");
+        correctCards[1].classList.add("checked");
+        correctCards[1].classList.remove("clicked");
+      } else {
+        const incorrectCards = document.querySelectorAll(".card.clicked");
+
+        incorrectCards[0].classList.add("shake");
+        incorrectCards[1].classList.add("shake");
+
+        setTimeout(() => {
+          incorrectCards[0].classList.remove("shake");
+          incorrectCards[0].classList.remove("clicked");
+          incorrectCards[1].classList.remove("shake");
+          incorrectCards[1].classList.remove("clicked");
+        }, 800);
+      }
+    }
+  });
+});
